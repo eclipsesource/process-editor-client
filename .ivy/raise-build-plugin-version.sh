7cefa56e6f0ae34f5a4f765b6dfdbd79a070db56
Louis <louis.mueller@axonivy.com>
2025-06-11 11:40:18 +0200

    remove project.build.plugin.version prop (#1021)

--- .ivy/raise-build-plugin-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn --batch-mode versions:set-property versions:commit -f playwright/process-test-project/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
-mvn --batch-mode versions:set-property versions:commit -f playwright/inscription-test-project/pom.xml -Dproperty=project.build.plugin.version -DnewVersion=${2} -DallowSnapshots=true
